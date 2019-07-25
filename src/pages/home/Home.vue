9a0a01912a49552bc17dd28b2784611cc13dd313
jingan <710694357@qq.com>
2019-07-26 03:00:05 +0800

    图标

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -4,6 +4,7 @@
   <div>
    <home-header></home-header>
    <home-swiper></home-swiper>
+   <home-icons></home-icons>
   </div>
 
 </template>
@@ -11,11 +12,12 @@
 <script>
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
-
+import HomeIcons from  './components/Icons'
 export default {
   components:{
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeIcons
   }
 }
 
---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,72 @@
+<template>
+<div class="icons">
+    <div class="icon">
+        <div class="icon-image">
+        <img class="icon-image-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+        </div>
+        <div class="icon-desc">热门景点</div>
+    </div>
+        <div class="icon">
+        <div class="icon-image">
+        <img class="icon-image-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+        </div>
+        <div class="icon-desc">热门景点</div>
+    </div>
+
+      
+
+</div>
+    
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+.icons{
+    overflow: hidden;
+    height:0;
+    padding-bottom:50%;
+   
+    //这样写， 宽高比为2
+}
+.icon{
+    position:relative;
+    overflow: hidden;
+    height: 0;
+    width:25%;
+    float:left;
+    padding-bottom: 25%;
+ 
+}
+.icon-image{
+    position: absolute;
+    top:0;
+    left:0;
+    right:0;
+    bottom: .44rem;
+    
+    box-sizing: border-box;
+    padding: .1rem;
+
+}
+.icon-image-content{
+    height: 100%;
+    display: block;
+    margin: auto;
+}
+.icon-desc{
+    position: absolute;
+    bottom:0;
+    left:0;
+    right:0;
+    height: .44rem;
+    line-height: .44rem;
+    color: #333;
+    text-align: center;
+
+}
+</style>
